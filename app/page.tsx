6623f1d9e0aa0f602bcba296e9dba22871ff9333
The Wizard of Hahz <info@luvnft.com>
2024-08-01 20:24:26 -0400

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -588,7 +588,7 @@ export default function Home() {
         </div>
         <div className="flex items-center gap-1">
           <Button title={t('discord')} variant="ghost" size="icon" className="h-8 w-8">
-            <a href="https://discord.gg/QQeFcekQyx" target="_blank">
+            <a href="https://discord.com/servers/atl5d-1244450286337003520" target="_blank">
               <Github className="h-5 w-5" />
             </a>
           </Button>
